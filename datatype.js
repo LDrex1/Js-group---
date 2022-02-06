82e436df18c1d6acbe04e44dee8f0c49457e0f79
LDrex1 <87782877+LDrex1@users.noreply.github.com>
2022-02-06 21:01:01 +0100

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,44 @@
+//page start
+alert("Assignments,  from 4-10");
+
+//number 4
+let x1, x2, x3, x4;
+x1 = "abcd";
+x2 = true;
+x3 = undefined;
+x4 = null;
+
+/* number 5, let's just assume this is
+also datatypes.js file instead of  
+creating too many files.
+*/
+console.log(typeof x1);
+console.log(typeof x2);
+console.log(typeof x3);
+console.log(typeof x4);
+
+//number 6
+let a1, a2, a3, a4;
+
+//number 7 and 8
+const firstName = "Damilare", lastName = "Abiola", maritalStatus = "single", country = "Nigeria", age = 22;
+
+//number 9
+const fN = "Damilare";
+const lN = "Abiola";
+const mS = "single";
+const Co = "Nigeria";
+const howOld = 22;
+
+//number 10
+let myAge;
+let yourAge;
+
+myAge = 5;
+yourAge = 10;
+
+console.log("My age is "+ myAge +" and your age is "+ yourAge);
+
+
+
+
